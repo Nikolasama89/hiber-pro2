c06a3f100075ac4973e6f0b501bef2d62312268d
Nikos <niqos89@gmail.com>
2025-04-25 21:02:07 +0300

    feat: queries

--- src/main/java/gr/aueb/cf/App.java
+++ src/main/java/gr/aueb/cf/App.java
@@ -7,8 +7,14 @@ import gr.aueb.cf.model.TeacherMoreInfo;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Root;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 public class App {
@@ -19,22 +25,75 @@ public class App {
 
         em.getTransaction().begin();
 
-        Teacher teacher = Teacher.builder()
-                .firstname("Νίκος")
-                .lastname("Μίχος")
-                .isActive(true)
-                .build();
+//        Teacher teacher = Teacher.builder()
+//                .firstname("Νίκος")
+//                .lastname("Μίχος")
+//                .isActive(true)
+//                .build();
+//
+//        TeacherMoreInfo teacherMoreInfo = TeacherMoreInfo.builder()
+//                .dateOfBirth(LocalDateTime.of(1989, 4, 21, 10, 10, 10))
+//                .gender(GenderType.MALE)
+//                .build();
+//
+//        teacher.setTeacherMoreInfo(teacherMoreInfo);
+//        Region region = em.find(Region.class, 1);
+//        teacher.addRegion(region);
+//
+//        em.persist(teacher);
+//        em.getTransaction().commit();
 
-        TeacherMoreInfo teacherMoreInfo = TeacherMoreInfo.builder()
-                .dateOfBirth(LocalDateTime.of(1989, 4, 21, 10, 10, 10))
-                .gender(GenderType.MALE)
-                .build();
+        // merge (update)
+//        String sql = "SELECT t from Teacher t WHERE t.lastname = :lastname";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        query.setParameter("lastname", "Ανδρούτσος");
+//        Teacher teacher = query.getSingleResult();
+//        teacher.setFirstname("Athanasios");
+//        em.merge(teacher);
 
-        teacher.setTeacherMoreInfo(teacherMoreInfo);
-        Region region = em.find(Region.class, 1);
-        teacher.addRegion(region);
+//        String sql = "SELECT t from Teacher t WHERE t.lastname = :lastname";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        query.setParameter("lastname", "Ανδρούτσος");
+//        Teacher teacher = query.getSingleResult();
+//        teacher.setIsActive(false);
+//        em.merge(teacher);
 
-        em.persist(teacher);
+        // SELECT τους active με region Θεσσαλονικη
+//        String sql = "SELECT t from Teacher t WHERE t.isActive = true AND t.region.title = :regionTitle";
+//        TypedQuery<Teacher> query = em.createQuery(sql, Teacher.class);
+//        query.setParameter("regionTitle", "Θεσσαλονίκη");
+//        List<Teacher> teachers = query.getResultList();
+//        teachers.forEach(System.out::println);
+
+        // count των teachers ανα περιοχη
+//        String sql = "SELECT r.title, COUNT(t) FROM Region r LEFT JOIN r.teachers t WHERE t.isActive = false OR t.isActive IS NULL GROUP BY r.title";
+//        TypedQuery<Object[]> query = em.createQuery(sql, Object[].class);
+//        List<Object[]> teachersPerRegion = query.getResultList();
+//        for (Object[] row : teachersPerRegion) {
+//            for (Object item: row) {
+//                System.out.print(item + " ");
+//            }
+//            System.out.println();
+//        }
+
+        // Find all active teachers in a region CRITERIA API
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Teacher> query = cb.createQuery(Teacher.class);
+        Root<Teacher> teacher = query.from(Teacher.class);
+        Join<Teacher, Region> region = teacher.join("region");
+
+        query.select(teacher).where(
+                cb.and(
+                        cb.isTrue(teacher.get("isActive")),
+                        cb.equal(region.get("title"), "Θεσσαλονίκη")
+                )
+        );
+        List<Teacher> teachers = em.createQuery(query).getResultList();
+        teachers.forEach(System.out::println);
+
+
+
+
         em.getTransaction().commit();
 
         em.close();
